57cf50355ef6bd88313b69317c84aa0c681ea44e
nsmenon <nsmenon@wpi.edu>
2019-02-14 12:27:14 -0500

    Added new LC for Spanish Classes

---
+++ src/edu/umass/ckc/wo/tutor/agent/LucasEmpathicLC.java
@@ -0,0 +1,21 @@
+package edu.umass.ckc.wo.tutor.agent;
+
+import edu.umass.ckc.wo.smgr.SessionManager;
+
+public class LucasEmpathicLC extends FullEmpathicLC {
+
+	public LucasEmpathicLC(SessionManager smgr) {}
+
+	public String getGender() {
+		return "male";
+	}
+
+	public String getEthnicity() {
+		return "brown";
+	}
+
+	public String getCharactersName() {
+		return "Lucas";
+	}
+
+}
