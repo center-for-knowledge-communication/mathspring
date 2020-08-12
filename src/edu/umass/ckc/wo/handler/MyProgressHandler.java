49b734b744a6eaff7e67a9ecff30f5a6d9df39cc
fsylvia <fsylvia13@gmail.com>
2020-08-12 16:33:09 -0400

    issue #194 fixed broken link to practice area

--- src/edu/umass/ckc/wo/handler/MyProgressHandler.java
+++ src/edu/umass/ckc/wo/handler/MyProgressHandler.java
@@ -35,7 +35,9 @@ import java.util.List;
  * Date: 8/16/12
  * Time: 3:30 PM
  * To change this template use File | Settings | File Templates.
- * Frank 05-18-2020 issue #125 cast elapsedtime from (double) to (int) - fixes url parameter of 'Logou' selection. 
+ * Frank 05-18-2020 issue #125 cast elapsedtime from (double) to (int) - fixes url parameter of 'Logou' selection.
+ * Frank 08-12-2020 issue #194 Send along the first unsolved problem encountered for 'Practice Area' link
+ 
  */
 public class MyProgressHandler {
 
@@ -97,6 +99,14 @@ public class MyProgressHandler {
             request.setAttribute("mastery", mastery);
             request.setAttribute("elapsedTime", (int)elapsedTime);    // DAM causes compile error
             request.setAttribute("topicId", topicId);
+            
+            // Send along the first unsolved problem encountered for 'Practice Area' link
+            for (problemDetails pd : problemDetailsList) {
+            	request.setAttribute("probId", pd.getProblemId());
+            	if (!pd.isSolved()) {
+            		break;
+            	}
+            }
 
             request.setAttribute("topicName", topicName);
 
